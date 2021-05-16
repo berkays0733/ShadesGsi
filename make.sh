6ee6760b36615d84bad1bcc60298b8a2d94279cf
Berkay AY <61064047+berkays0733@users.noreply.github.com>
2021-05-16 12:11:16 +0300

    Optimize Source

--- make.sh
+++ make.sh
@@ -39,7 +39,7 @@ do
     fi
 done
 if [ "$flag" == "false" ]; then
-    echo "$romtype is not supported rom, supported roms:"
+    echo "$romtype is not supported, supported roms:"
     for dir in "${roms[@]}"
     do
         ver=`echo "$dir" | rev | cut -d "/" -f 2 | rev`
@@ -54,7 +54,7 @@ case "$outputtype" in
     *"Aonly"*) flag=true ;;
 esac
 if [ "$flag" == "false" ]; then
-    echo "$outputtype is not supported type, supported types:"
+    echo "$outputtype is not supported, supported types:"
     echo "AB"
     echo "Aonly"
     exit 1
@@ -80,16 +80,15 @@ romsdir="$LOCALDIR/roms"
 prebuiltdir="$LOCALDIR/prebuilt"
 scriptsdir="$LOCALDIR/scripts"
 
-echo "Create Temp dir"
 rm -rf $tempdir
 mkdir -p "$systemdir"
 
 if [ "$sourcetype" == "Aonly" ]; then
-    echo "Warning: Aonly source detected, using P AOSP ramdisk"
+    echo "using P AOSP ramdisk"
     cd "$systemdir"
     tar xf "$prebuiltdir/ABrootDir.tar"
     cd "$LOCALDIR"
-    echo "Making copy of source rom to temp"
+    echo "Copying to temp"
     ( cd "$sourcepath" ; sudo tar cf - . ) | ( cd "$systemdir/system" ; sudo tar xf - ) &> /dev/null
     cd "$LOCALDIR"
     sed -i "/ro.build.system_root_image/d" "$systemdir/system/build.prop"
@@ -120,7 +119,7 @@ fi
 # Detect is the src treble ro.treble.enabled=true
 istreble=`cat $systemdir/system/build.prop | grep ro.treble.enabled | cut -d "=" -f 2`
 if [[ ! "$istreble" == "true" ]]; then
-    echo "source have treble line deleted, its ok"
+    echo "Not trebled but OK"
 fi
 
 # Detect Source API level
@@ -146,13 +145,13 @@ fi
 
 # Detect rom folder again
 if [[ ! -d "$romsdir/$sourcever/$romtype" ]]; then
-    echo "$romtype is not supported rom for android $sourcever"
+    echo "$romtype is not supported for $sourcever"
     exit 1
 fi
 
 # Detect arch
 if [[ ! -f "$systemdir/system/lib64/libandroid.so" ]]; then
-    echo "32bit source detected, weird flex but ok!"
+    echo "32bit source detected !"
     # do something here?
 fi
 
@@ -162,7 +161,7 @@ $romsdir/$sourcever/$romtype/$romtypename/debloat.sh "$systemdir/system" 2>/dev/
 
 
 # Start patching
-echo "Patching started..."
+
 cp trebleRom.apk $systemdir/system/app
 cp telegram.apk $systemdir/system/app
 $scriptsdir/fixsymlinks.sh "$systemdir/system" 2>/dev/null
@@ -235,14 +234,14 @@ bytesToHuman() {
     done
     echo "$b$d ${S[$s]}"
 }
-echo "Raw Image Size: $(bytesToHuman $systemsize)" >> "$outputinfo"
+echo "Image Size: $(bytesToHuman $systemsize)" >> "$outputinfo"
 
-echo "Creating Image: $outputimagename"
+echo "Generating GSI: $outputimagename"
 # Use ext4fs to make image in P or older!
 if [ "$sourcever" == "9" ]; then
     useold="--old"
 fi
 $scriptsdir/mkimage.sh $systemdir $outputtype $systemsize $output $useold > $tempdir/mkimage.log
 
-echo "Remove Temp dir"
+
 rm -rf "$tempdir"
